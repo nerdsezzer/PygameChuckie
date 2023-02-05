de69fcf36681994ea383b557337a11dea3b5091a
nerdSezzer <sezzeriam@gmail.com>
2023-02-05 09:21:31 +0000

    pygame-able.

--- chuckie/layout_elements.py
+++ chuckie/layout_elements.py
@@ -10,12 +10,10 @@ class Egg(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load(os.path.join('.', 'images', 'egg.png')).convert()
-        #img.convert_alpha()
-        #img.set_colorkey(ALPHA)
-        #print(f"putting an egg at [{x}, {y}]")
         self.rect = self.image.get_rect()
         self.rect.x = x * config.tile_width
         self.rect.y = y * config.tile_height
+        self.name = "egg"
         return
 
 
@@ -24,12 +22,10 @@ class Grain(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load(os.path.join('.', 'images', 'grain.png')).convert()
-        #img.convert_alpha()
-        #img.set_colorkey(ALPHA)
-        #print(f"putting grain at [{x}, {y}]")
         self.rect = self.image.get_rect()
-        self.rect.x = x *config.tile_width
-        self.rect.y = y *config.tile_height
+        self.rect.x = x * config.tile_width
+        self.rect.y = y * config.tile_height
+        self.name = "grain"
         return
 
 
@@ -38,12 +34,10 @@ class Floor(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load(os.path.join('.', 'images', 'floor.png')).convert()
-        #img.convert_alpha()
-        #img.set_colorkey(ALPHA)
-        #print(f"putting floor at [{x}, {y}]")
         self.rect = self.image.get_rect()
-        self.rect.x = x *config.tile_width
-        self.rect.y = y *config.tile_height
+        self.rect.x = x * config.tile_width
+        self.rect.y = y * config.tile_height
+        self.name = "floor"
         return
 
 
@@ -52,10 +46,8 @@ class Ladder(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load(os.path.join('.', 'images', 'ladder.png')).convert()
-        #img.convert_alpha()
-        #img.set_colorkey(ALPHA)
-        #print(f"putting ladder at [{x}, {y}]")
         self.rect = self.image.get_rect()
-        self.rect.x = x *config.tile_width
-        self.rect.y = y *config.tile_height
+        self.rect.x = x * config.tile_width
+        self.rect.y = y * config.tile_height
+        self.name = "ladder"
         return
