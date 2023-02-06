20f471a66ed4372379125848a3733670c812b086
nerdSezzer <sezzeriam@gmail.com>
2023-02-06 16:46:36 +0000

    Fixed lift ... it goes up and everything!

--- chuckie/lift.py
+++ chuckie/lift.py
@@ -1,23 +1,23 @@
-#import turtle
 import pygame
 import os
 
 import config
-from config import global_w, global_h
 from chuckie.thing import Thing
 import chuckie.utils as utils
 
 
 class Lift(Thing):
 
+    LIFT_DISAPPEARS_AT = (6 * config.tile_height)
+    LIFT_REAPPEARS_AT = (27 * config.tile_height)
+
     def __init__(self, level, start_tile_x, start_tile_y, direction):
         super().__init__('lift', level)
 
         file = "lift-left.png" if direction == "left" else "lift-right.png"
         self.image = pygame.image.load(os.path.join('.', 'images', file)).convert()
-        self.image.convert_alpha()
-        self.image.set_colorkey((0, 0, 0))
-        print(f"putting a lift at [{start_tile_x}, {start_tile_y}]")
+        if config.debug_lifts:
+            print(f"putting a lift at [{start_tile_x}, {start_tile_y}]")
         self.rect = self.image.get_rect()
         self.rect.x = start_tile_x * config.tile_width
         self.rect.y = start_tile_y * config.tile_height
@@ -27,6 +27,10 @@ class Lift(Thing):
         self.previous_direction = "still"
         (self.hx, self.hy) = utils.tile_to_real(start_tile_x, start_tile_y)
         return
+
+    def __repr__(self):
+        tx, ty = utils.real_to_tile(self.rect.x, self.rect.y)
+        return f"<Lift ({self.rect.x:.2f}, {self.rect.y:.2f}) [{tx},{ty}] {self.direction})>"
 
     @property
     def hy(self):
@@ -44,8 +48,8 @@ class Lift(Thing):
         bottom of the screen.
         """
         self.hy += config.lift_default_hy_velocity
-        if self.hy < (6*config.tile_height):
-            self.hy = (27*config.tile_height)
+        if self.hy < Lift.LIFT_DISAPPEARS_AT:
+            self.hy = Lift.LIFT_REAPPEARS_AT
 
         if config.debug_lifts:
             self.dump_state("lift: ")
