0a58c8e72d648989b719da68f02948a910d5ee99
nerdSezzer <sezzeriam@gmail.com>
2023-02-12 15:25:55 +0000

    tiny change to test linux setup.

--- chuckie/hen.py
+++ chuckie/hen.py
@@ -37,7 +37,7 @@ class Hen(Thing):
         if config.debug_hens:
             print(f"putting an hen at [{start_tile_x}, {start_tile_y}] => {id(self)}")
 
-        self.hx_velocity = config.hen_default_hx_velocity if direction == "right" else 0-config.hen_default_hx_velocity
+        self.dx = config.hen_default_hx_velocity if direction == "right" else 0-config.hen_default_hx_velocity
 
         self.random = Random()
         self.random.seed()
@@ -70,10 +70,10 @@ class Hen(Thing):
             self.images = self.images_eating
             self.image = self.images[self.frame]
             if self.direction == 'left' and self.frame <= 3:
-                self.rect.x -= (1*config.tile_width)
+                self.rect.x -= config.tile_width
             if self.frame == 3:
                 self.state = 'walking'
-                self.hx_velocity = config.hen_default_hx_velocity if self.direction == 'right' else 0-config.hen_default_hy_velocity
+                self.dx = config.hen_default_hx_velocity if self.direction == 'right' else 0-config.hen_default_hy_velocity
             if self.direction == 'right':
                 self.image = self.images[self.frame]
             elif self.direction == 'left':
@@ -95,29 +95,29 @@ class Hen(Thing):
     def move_up(self):
         if self.check_can_move_up_down():
             self.direction = "up"
-            self.hx_velocity = 0
-            self.hy_velocity = 0 - config.hen_default_hy_velocity
+            self.dx = 0
+            self.dy = 0 - config.hen_default_hy_velocity
         return
 
     def move_down(self):
         if self.check_can_move_up_down():
             self.direction = "down"
-            self.hx_velocity = 0
-            self.hy_velocity = config.hen_default_hy_velocity
+            self.dx = 0
+            self.dy = config.hen_default_hy_velocity
         return
 
     def move_left(self):
         if self.check_can_move_sideways():
             self.direction = "left"
-            self.hx_velocity = 0 - config.hen_default_hx_velocity
-            self.hy_velocity = 0
+            self.dx = 0 - config.hen_default_hx_velocity
+            self.dy = 0
         return
 
     def move_right(self):
         if self.check_can_move_sideways():
             self.direction = "right"
-            self.hx_velocity = config.hen_default_hx_velocity
-            self.hy_velocity = 0
+            self.dx = config.hen_default_hx_velocity
+            self.dy = 0
         return
 
     def check_can_move_sideways(self) -> bool:
@@ -127,8 +127,7 @@ class Hen(Thing):
         It's overriden here because we already know what moves are possible
         for Hen's, so all we need to do if check they're on the top of a block.
         """
-        (tx, ty) = utils.real_to_tile(self.hx, self.hy)
-        if not utils.top_of_block(self.hy):
+        if not utils.top_of_block(self.y):
             return False
         return True
 
@@ -137,8 +136,7 @@ class Hen(Thing):
         Checks to see if a move up and down is possible, i.e. a ladder.
         This function takes the current direction of travel into account.
         """
-        (tx, ty) = utils.real_to_tile(self.hx, self.hy)
-        if not utils.middle_of_block(self.hx):
+        if not utils.middle_of_block(self.x):
             return False
         return True
 
@@ -148,19 +146,15 @@ class Hen(Thing):
         (thanks Dan!)
         """
         if self.state != 'eating':
-            # our current tile....
-            (tx, ty) = utils.real_to_tile(self.hx, self.hy)
-
             # just check we're actually going over a tile boundary...
             # if not process the move, without thinking too hard!
-            if not utils.top_of_block(self.hy) or not utils.middle_of_block(self.hx):
-                self.hx += self.hx_velocity
-                self.hy += self.hy_velocity
+            if not utils.top_of_block(self.y) or not utils.middle_of_block(self.x):
+                self.x += self.dx
+                self.y += self.dy
                 self.draw()
                 return
 
             # get all possible directions we can move in for the current tile.
-            (tx, ty) = utils.real_to_tile(self.hx, self.hy)
             possible = self.get_possible_moves()
             if sum(possible) == 0:
                 return
@@ -177,13 +171,13 @@ class Hen(Thing):
             if options == 1:
                 func = self.actions[self.choose(possible)]
                 func()
-            elif options == 2 and self.hx_velocity > 0 and can_go_right:
+            elif options == 2 and self.dx > 0 and can_go_right:
                 self.move_right()
-            elif options == 2 and self.hx_velocity < 0 and can_go_left:
+            elif options == 2 and self.dx < 0 and can_go_left:
                 self.move_left()
-            elif options == 2 and self.hy_velocity < 0 and can_go_up:
+            elif options == 2 and self.dy < 0 and can_go_up:
                 self.move_up()
-            elif options == 2 and self.hy_velocity > 0 and can_go_down:
+            elif options == 2 and self.dy > 0 and can_go_down:
                 self.move_down()
             elif options == 0:
                 if config.debug_hens:
@@ -192,20 +186,20 @@ class Hen(Thing):
                 # fix it for Dan...
                 # if they're walking in one direction, they won't just turn round
                 # at a junction.
-                if can_go_left and self.hx_velocity < 0:
+                if can_go_left and self.dx < 0:
                     possible[3] = False
-                if can_go_right and self.hx_velocity > 0:
+                if can_go_right and self.dx > 0:
                     possible[2] = False
-                if can_go_up and self.hy_velocity > 0:
+                if can_go_up and self.dy > 0:
                     possible[1] = False
-                if can_go_down and self.hy_velocity < 0:
+                if can_go_down and self.dy < 0:
                     possible[0] = False
 
                 # another fix for Dan...
                 # if they are coming off a ladder, try and go the same direction
                 # as they were going before they got on the ladder.
                 if sum(possible) > 1:
-                    if self.hy_velocity != 0:  # we were going up or down.
+                    if self.dy != 0:  # we were going up or down.
                         if can_go_left and self.direction == 'left':
                             possible[3] = False
                         if can_go_right and self.direction == 'right':
@@ -215,21 +209,20 @@ class Hen(Thing):
                 func()
 
             if self.is_going_left():
-                next_tile = (self.hx + self.hx_velocity, self.hy + self.hy_velocity + config.tile_height)
+                next_tile = (self.x + self.dx, self.y + self.dy + config.tile_height)
             else:
-                next_tile = (self.hx + (1*config.tile_width), self.hy + self.hy_velocity + config.tile_height)
+                next_tile = (self.x + config.tile_width, self.y + self.dy + config.tile_height)
             next_element = next(iter([r for r in self.level.elements if r.rect.collidepoint(next_tile)]), None)
             if next_element and next_element.name == 'grain':
                 self.state = 'eating'
-                print(f"about to be 'eating' setting state to {self.state}")
                 self.level.consume_grain(next_element, hen_mode=True)
-                self.hx_velocity = 0
-                self.hy_velocity = 0
-                self.frame = -1  # reset the step counter for eating.
+                self.dx = 0
+                self.dy = 0
+                self.frame = -1  # reset the frame counter for eating.
                 self.draw()
                 return
 
-        self.hx += self.hx_velocity
-        self.hy += self.hy_velocity
+        self.x += self.dx
+        self.y += self.dy
         self.draw()
         return
--- chuckie/sounds.py
+++ chuckie/sounds.py
@@ -27,22 +27,23 @@ class SoundsThread(threading.Thread):
     def __init__(self):
         super().__init__(daemon=True)
         pygame.midi.init()
-        print(f"number of midi devices = {pygame.midi.get_count()}")
+        # print(f"number of midi devices = {pygame.midi.get_count()}")
         device = pygame.midi.get_default_output_id()
         if device != -1:
-            print(f"default midi device = {device}")
+            # print(f"default midi device = {device}")
             self.player = pygame.midi.Output(device)
             self.player.set_instrument(35)
         else:
             self.player = None
-        self.walking = Parameters(False, 74)
+
+        self.params = Parameters(False, 74)
         return
 
     def walking_on(self, harry):
         if self.player:
             delta = 0
             if harry.state == 'falling':
-                note = self.walking.note - 1
+                note = self.params.note - 1
             elif harry.y_velocity == 0:
                 if harry.direction == 'up' or harry.direction == 'down':
                     note = 84
@@ -52,26 +53,25 @@ class SoundsThread(threading.Thread):
                 delta = harry.y_velocity if harry.y_velocity > 0 else (0 - harry.y_velocity)
                 note = 74 + (15 - delta)//2
 
-            print(f"delta = {delta}, jump_height = {config.jump_height}, note = {note}")
-            self.walking.update(True, note)
+            # print(f"delta = {delta}, jump_height = {config.jump_height}, note = {note}")
+            self.params.update(True, note)
         return
 
     def consume(self, item: str):
         if self.player:
-            self.walking.update(True, 54)
+            self.params.update(True, 54)
         return
 
     def walking_off(self):
         if self.player:
-            self.walking.update(False, 74)
+            self.params.update(False, 74)
         return
 
     def run(self):
         if self.player:
-            print("thread starting")
             while True:
-                if self.walking.value:
-                    note = self.walking.note
+                if self.params.value:
+                    note = self.params.note
                     self.player.note_on(note, 64)
                     time.sleep(0.08)
                     self.player.note_off(note, 64)
