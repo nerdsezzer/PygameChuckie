7e4077b7a1a89f82b28e4877afc0ff4d8d8fc517
nerdSezzer <sezzeriam@gmail.com>
2023-02-16 15:37:37 +0000

    removed redundant functions, what was i thinking!

--- chuckie/controls.py
+++ chuckie/controls.py
@@ -6,7 +6,7 @@ from config import start_paused
 
 class Controls:
     """
-    Class to manage the window hooks for key presses and key releases.
+    Class to collate key presses and key releases.
     """
 
     def __init__(self):
@@ -42,67 +42,27 @@ class Controls:
 
             if event.type == pygame.KEYDOWN:
                 if event.key == ord('p') or event.key == pygame.K_ESCAPE:
-                    self.toggle_pause()
+                    self.paused = not self.paused
+                    print(f"paused {self.paused}")
                 if event.key == ord('w'):
-                    self.move_up()
+                    self.w_down = True
                 if event.key == ord('s'):
-                    self.move_down()
+                    self.s_down = True
                 if event.key == ord('a'):
-                    self.move_left()
+                    self.a_down = True
                 if event.key == ord('d'):
-                    self.move_right()
+                    self.d_down = True
                 if event.key == pygame.K_SPACE:
-                    self.move_jump()
+                    self.space_down = True
 
             if event.type == pygame.KEYUP:
                 if event.key == ord('w'):
-                    self.move_up_end()
+                    self.w_down = False
                 if event.key == ord('s'):
-                    self.move_down_end()
+                    self.s_down = False
                 if event.key == ord('a'):
-                    self.move_left_end()
+                    self.a_down = False
                 if event.key == ord('d'):
-                    self.move_right_end()
+                    self.d_down = False
 
-        return
-
-    def toggle_pause(self):
-        self.paused = not self.paused
-        print(f"paused {self.paused}")
-        return
-
-    def move_left(self):
-        self.a_down = True
-        return
-
-    def move_right(self):
-        self.d_down = True
-        return
-
-    def move_up(self):
-        self.w_down = True
-        return
-
-    def move_down(self):
-        self.s_down = True
-        return
-
-    def move_left_end(self):
-        self.a_down = False
-        return
-
-    def move_right_end(self):
-        self.d_down = False
-        return
-
-    def move_up_end(self):
-        self.w_down = False
-        return
-
-    def move_down_end(self):
-        self.s_down = False
-        return
-
-    def move_jump(self):
-        self.space_down = True
         return
