945dc1446773f52dc72ef04072a25d7cbbfbe3c9
nerdSezzer <sezzeriam@gmail.com>
2023-02-16 15:55:37 +0000

    move(), updates rect.y value.

--- chuckie/lift.py
+++ chuckie/lift.py
@@ -2,8 +2,7 @@ import pygame
 import os
 
 import config
-from chuckie.thing import Thing
-import chuckie.utils as utils
+from chuckie.thing import Thing, DIR
 
 
 class Lift(Thing):
@@ -11,10 +10,10 @@ class Lift(Thing):
     LIFT_DISAPPEARS_AT = (6 * config.tile_height)
     LIFT_REAPPEARS_AT = (27 * config.tile_height)
 
-    def __init__(self, level, start_tile_x, start_tile_y, direction):
+    def __init__(self, level, start_tile_x, start_tile_y, direction: DIR):
         super().__init__('lift', level, start_tile_x, start_tile_y, direction)
 
-        file = "lift-left.png" if direction == "left" else "lift-right.png"
+        file = "lift-left.png" if direction == DIR.LEFT else "lift-right.png"
         self.image = pygame.image.load(os.path.join('.', 'images', file)).convert()
 
         if config.debug_lifts:
@@ -29,17 +28,18 @@ class Lift(Thing):
         return "[" + name + "]" + rest[6:]
 
     def __repr__(self):
-        tx, ty = utils.real_to_tile(self.x, self.hy)
-        return f"<Lift ({self.x}, {self.hy}) [{tx},{ty}] speed = {self.dy}, {self.direction})>"
+        return f"<Lift ({self.x}, {self.y}) [{self.tx},{self.ty}] speed = {self.dy}, {self.direction})>"
 
     def move(self):
         """
         move the lifts, upwards.  If they reach the top, they respawn at the
         bottom of the screen.
         """
-        self.y += config.lift_default_hy_velocity
+        self.y += config.lift_hy_velocity
         if self.y < Lift.LIFT_DISAPPEARS_AT:
             self.y = Lift.LIFT_REAPPEARS_AT
+
+        self.rect.y = self.y
 
         if config.debug_lifts:
             print(self)
