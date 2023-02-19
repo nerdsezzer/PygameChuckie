f0e68e81d4ebf6d382ebe46df9aff79ac7a8fbef
nerdSezzer <sezzeriam@gmail.com>
2023-02-19 16:53:19 +0000

    with "Game Over" and "Well Done" screens.

--- chuckie_egg.py
+++ chuckie_egg.py
@@ -27,7 +27,7 @@ if debug_display:
     print(f"tile width={tile_width}, height={tile_height}")
 
 # Ok, lets do this...
-status = Status(window)
+status = Status()
 ctrls = Controls()
 level = Level(levels[status.game_level], status)
 level.create()
@@ -37,22 +37,34 @@ level.create()
 # Util functions
 # -----------------------------------------------------------------------------
 
-
-def get_ready_screen():
+def _screen(text_one: str, text_two: str = None):
     delay = 40
     while delay:
-        # print(delay)
         time.sleep(0.05)
         window.fill([0, 0, 0])
         font = pygame.font.SysFont('Arial', 36)
-        text_surface = font.render("Get Ready", False, pygame.color.Color('yellow'))
+        text_surface = font.render(text_one, False, pygame.color.Color('yellow'))
         window.blit(text_surface, (550, 420))
-        text_surface = font.render("Player 1", False, pygame.color.Color('cyan'))
-        window.blit(text_surface, (565, 480))
+        if text_two:
+            text_surface = font.render(text_two, False, pygame.color.Color('cyan'))
+            window.blit(text_surface, (565, 480))
         pygame.display.flip()
         clock.tick(fps)
         delay -= 1
     return
+
+
+def get_ready_screen():
+    return _screen("Get Ready", "Player 1")
+
+
+def oh_dear_screen():
+    return _screen("Game Over")
+
+
+def all_done_screen():
+    return _screen("Well Done")
+
 
 # -----------------------------------------------------------------------------
 # main loop
@@ -62,6 +74,8 @@ def get_ready_screen():
 sounds = SoundsThread()
 sounds.start()
 
+pygame.time.set_timer(ctrls.HALF_SECOND_TICK, 500)
+
 all_done = False
 tick = False
 
@@ -69,7 +83,7 @@ while not all_done:
 
     while status.game_lives > 0:
 
-        ctrls.process_events()
+        ctrls.process_events(status.time_tick)
         if not ctrls.paused:
 
             # update level's movables
@@ -81,21 +95,23 @@ while not all_done:
                     or level.check_lift_death() \
                     or level.check_collision() \
                     or status.is_time_up():
-                print("Oopsie!")
-                status.game_lives -= 1
+
+                if not status.on_harry_died():
+                    break
+
                 # or stall for 2 seconds
                 get_ready_screen()
                 level.reset()
 
             # check we've completed the level?
             if level.are_all_eggs_collected():
-                print("Yay!")
-                status.update_score_end_of_level(window)
+                status.update_score_end_of_level(window, level, clock, fps)
                 level.unload()
 
                 # have we completed the game?
                 status.game_level += 1
                 if status.game_level >= len(levels):
+                    all_done_screen()
                     all_done = True
                     break
 
@@ -107,22 +123,18 @@ while not all_done:
                 level.create()
                 break
 
-            status.update()
-
         # normal game tick,d
         window.fill([0, 0, 0])
-        status.draw()
+        status.draw(window)
         level.draw(window)
         pygame.display.flip()
         clock.tick(fps)
 
     # ah, sad face...
     if status.game_lives == 0:
-        print("game over!")
+        oh_dear_screen()
         break
 
-
-print("bye!")
 del sounds.player
 pygame.midi.quit()
 
