0e27b1241891753f6372510ac61de8e576691d84
NerdSezzer <sezzeriam@gmail.com>
2023-02-21 17:14:02 +0000

    decoupled some bits, recoupled some bits, general tidying of main loop.

--- chuckie_egg.py
+++ chuckie_egg.py
@@ -14,11 +14,10 @@ from high_scores import HighScores
 
 # Set up the game window
 window = pygame.display.set_mode([config.window_width, config.window_height])
-clock = pygame.time.Clock()
+pygame.display.set_caption("nerdSezzer - ChuckieEgg 2023!")
 pygame.init()
-fps = 20
 
-font = pygame.font.Font(os.path.join('.', config.font_name), 36)
+clock = pygame.time.Clock()
 
 if debug_display:
     """
@@ -28,131 +27,105 @@ if debug_display:
     print(f"Screen width={window.get_width()}, height={window.get_height()}")
     print(f"tile width={tile_width}, height={tile_height}")
 
-# Ok, lets do this...
-status = Status(font)
+status = Status()
 ctrls = Controls()
-high_scores = HighScores()
+high_scores = HighScores(window, clock)
 
 pygame.mixer.init()
 opps = pygame.mixer.Sound(os.path.join('.', 'opps.wav'))
-start = pygame.mixer.Sound(os.path.join('.', 'start.wav'))
 
 
 # -----------------------------------------------------------------------------
 # Util functions
 # -----------------------------------------------------------------------------
 
-def _screen(text_one: str, text_two: str = None, take_input: bool = False):
+def get_ready_screen():
     delay = 40
+    font = pygame.font.Font(os.path.join('.', config.font_name), 36)
     window.fill([0, 0, 0])
     while delay:
         time.sleep(0.05)
 
-        text_surface = font.render(text_one, False, pygame.color.Color('yellow'))
+        text_surface = font.render("Get Ready", False, pygame.color.Color('yellow'))
         x = (window.get_width()-text_surface.get_width()) // 2
         window.blit(text_surface, (x, 420))
 
-        if text_two:
-            text_surface = font.render(text_two, False, pygame.color.Color('cyan'))
-            x = (window.get_width()-text_surface.get_width()) // 2
-            window.blit(text_surface, (x, 480))
+        text_surface = font.render("Player 1", False, pygame.color.Color('cyan'))
+        x = (window.get_width()-text_surface.get_width()) // 2
+        window.blit(text_surface, (x, 480))
 
         delay -= 1
         pygame.display.flip()
-        clock.tick(fps)
+        clock.tick(config.fps)
     return
-
-
-def get_ready_screen():
-    return _screen("Get Ready", "Player 1")
-
-
-def oh_dear_screen():
-    return _screen("Game Over")
-
-
-def all_done_screen():
-    return high_scores.well_done_screen(window, font, clock, fps)
 
 
 # -----------------------------------------------------------------------------
 # main loop
 # -----------------------------------------------------------------------------
 
-
+# Use a time to ensure more accurate ticks for the game time.
 pygame.time.set_timer(ctrls.HALF_SECOND_TICK, 500)
 
-all_done = False
 tick = False
 
+# create level one.
 level = Level(levels[status.game_level], status)
 level.create()
 
-while not all_done:
+# display the high scores, and wait for the 'S' key to start.
+high_scores.display()
 
-    high_scores.high_scores_screen(ctrls, window, font, clock, fps)
-    pygame.mixer.Sound.play(start)
+while status.game_lives > 0:
 
-    while status.game_lives > 0:
+    ctrls.process_events(status.time_tick)
+    if not ctrls.paused:
 
-        ctrls.process_events(status.time_tick)
-        if not ctrls.paused:
+        # update level's movables
+        tick = level.update_hens(tick)
+        level.update_lifts()
 
-            # update level's movables
-            tick = level.update_hens(tick)
-            level.update_lifts()
+        # update Harry's location, and check for death!
+        if not level.harry.move(ctrls) \
+                or level.check_lift_death() \
+                or level.check_collision() \
+                or status.is_time_up():
 
-            # update Harry's location, and check for death!
-            if not level.harry.move(ctrls) \
-                    or level.check_lift_death() \
-                    or level.check_collision() \
-                    or status.is_time_up():
+            opps.play()
 
-                pygame.mixer.Sound.play(opps)
-                if not status.on_harry_died():
-                    break
-
-                # or stall for 2 seconds
+            # check to see if there are any more lives left...
+            # ... if so, display the 'Get Ready' screen and reset the level.
+            if status.on_harry_died():
                 get_ready_screen()
                 level.reset()
-                pygame.mixer.Sound.play(start)
 
-            # check we've completed the level?
-            if level.are_all_eggs_collected():
-                status.update_score_end_of_level(window, level, clock, fps)
-                level.unload()
+        # check we've completed the level?
+        if level.are_all_eggs_collected():
+            status.end_of_level(window, level, clock)
+            level.unload()
 
-                # have we completed the game?
-                status.game_level += 1
-                if status.game_level >= len(levels):
-                    name = all_done_screen()
-                    high_scores.update_high_scores(name, status.game_score)
-                    ctrls = Controls()
-                    high_scores.high_scores_screen(ctrls, window, font, clock, fps)
-                    pygame.mixer.Sound.play(start)
-                    status.game_level = 0
-                else:
-                    # stall for a bit...
-                    get_ready_screen()
+            # have we completed the game...
+            status.game_level += 1
+            if status.game_level >= len(levels):
+                high_scores.update(status.game_score)
+                status.reset_game()
+            else:
+                # ... or just the level?
+                get_ready_screen()
 
-                # recreate Level with the next lot of game data.
-                level = Level(levels[status.game_level], status)
-                level.create()
-                pygame.mixer.Sound.play(start)
-                break
+            # recreate ctrls (to reset values), and create the next level.
+            ctrls = Controls()
+            level = Level(levels[status.game_level], status)
+            level.create()
 
-        # normal game tick,d
-        window.fill([0, 0, 0])
-        status.draw(window)
-        level.draw(window)
-        pygame.display.flip()
-        clock.tick(fps)
+    # normal game tick,d
+    window.fill([0, 0, 0])
+    status.draw(window)
+    level.draw(window)
+    pygame.display.flip()
+    clock.tick(config.fps)
 
-    # ah, sad face...
-    if status.game_lives == 0:
-        oh_dear_screen()
-        break
-
+# all done.
 pygame.midi.quit()
 
 
