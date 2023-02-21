55094592abea03ef97ff33976086fef044f19108
NerdSezzer <sezzeriam@gmail.com>
2023-02-21 17:10:22 +0000

    updated interface and added __main__ test

--- high_scores.py
+++ high_scores.py
@@ -1,4 +1,3 @@
-import time
 import pygame
 import os
 import sys
@@ -7,16 +6,38 @@ import config
 
 
 class HighScores:
+    """
+    Class to manage the high scores list, just like on the Acorn, the list doesn't
+    (yet?) persist outside of runtime.
+    """
 
     default_scores = [('Nigel A.', 198300), ('A&F', 80000), ('A&F', 70000), ('A&F', 60000),
                       ('A&F', 50000), ('A&F', 40000), ('A&F', 30000), ('A&F', 20000),
                       ('A&F', 10000), ('nerdSezzer', 5000)]
 
-    def __init__(self):
+    def __init__(self, _surface: pygame.Surface, _clock: pygame.time):
+        self.surface = _surface
+        self.clock = _clock
         self.scores = HighScores.default_scores
+        self.font = pygame.font.Font(os.path.join('.', config.font_name), 36)
         return
 
-    def update_high_scores(self, new_name, new_score):
+    def update(self, new_score):
+        """
+        Display the 'well done' screen, and get the input for name, add
+        the new name and score into the scores, and display them using
+        a new Controls (to reset keypress buffers) and wait for 's' to start.
+        """
+        name = self.well_done_screen()
+        if name:
+            self.add_new_score(name, new_score)
+        self.display()
+        return
+
+    def add_new_score(self, new_name, new_score):
+        """
+        Inserts the new name and score into the high scores list.
+        """
         for name, score in self.scores:
             if new_score > score:
                 x = self.scores.index((name, score))
@@ -28,7 +49,7 @@ class HighScores:
         return
 
     @staticmethod
-    def process_events_for_input():
+    def process_events():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -47,24 +68,29 @@ class HighScores:
                 return event.unicode
         return
 
-    def well_done_screen(self, window, font, clock, fps):
-        delay = 40
+    def well_done_screen(self):
+        """
+        Display 'well done' and use a specific Controls capture input as name for high score.
+        """
+        self.surface.fill([0, 0, 0])
         name = ""
+
         take_input = True
-        window.fill([0, 0, 0])
-        while delay:
-            time.sleep(0.05)
+        while take_input:
+            text_surface = self.font.render("Well Done", False, pygame.color.Color('yellow'))
+            x = (self.surface.get_width() - text_surface.get_width()) // 2
+            self.surface.blit(text_surface, (x, 420))
 
-            text_surface = font.render("Well Done", False, pygame.color.Color('yellow'))
-            x = (window.get_width() - text_surface.get_width()) // 2
-            window.blit(text_surface, (x, 420))
+            text_surface = self.font.render("or press Esc to skip", False, pygame.color.Color('yellow'))
+            x = (self.surface.get_width() - text_surface.get_width()) // 2
+            self.surface.blit(text_surface, (x, 540))
 
-            text_surface = font.render("Please enter your name:", False, pygame.color.Color('cyan'))
-            window.blit(text_surface, (300, 480))
+            text_surface = self.font.render("Please enter your name:", False, pygame.color.Color('cyan'))
+            self.surface.blit(text_surface, (300, 480))
 
             x = 300 + text_surface.get_width() + 20
             if take_input:
-                key = self.process_events_for_input()
+                key = self.process_events()
                 if key:
                     if key == -1:
                         take_input = False
@@ -72,63 +98,83 @@ class HighScores:
                     elif key == -2:
                         take_input = False
                     elif key == -3:
-                        text_surface = font.render(name, False, pygame.color.Color('black'))
-                        window.blit(text_surface, (x, 480))
+                        text_surface = self.font.render(name, False, pygame.color.Color('black'))
+                        self.surface.blit(text_surface, (x, 480))
                         name = name[:-1]
-                        text_surface = font.render(name, False, pygame.color.Color('white'))
-                        window.blit(text_surface, (x, 480))
+                        text_surface = self.font.render(name, False, pygame.color.Color('white'))
+                        self.surface.blit(text_surface, (x, 480))
                     else:
-                        text_surface = font.render(name, False, pygame.color.Color('black'))
-                        window.blit(text_surface, (x, 480))
-                        name += key
-                        text_surface = font.render(name, False, pygame.color.Color('white'))
-                        window.blit(text_surface, (x, 480))
-            else:
-                delay -= 1
+                        text_surface = self.font.render(name, False, pygame.color.Color('black'))
+                        self.surface.blit(text_surface, (x, 480))
+                        name += str(key)
+                        text_surface = self.font.render(name, False, pygame.color.Color('white'))
+                        self.surface.blit(text_surface, (x, 480))
 
             pygame.display.flip()
-            clock.tick(fps)
+            self.clock.tick(config.fps)
         return name
 
-    def high_scores_screen(self, ctrls, window, font, clock, fps):
-        while ctrls.paused:
-            time.sleep(0.05)
-            window.fill([0, 0, 0])
+    @staticmethod
+    def wait_for_s_key():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                try:
+                    sys.exit()
+                finally:
+                    print("done.")
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == ord('s'):
+                    return False
+        return True
+
+    def display(self):
+        """
+        Display the high scores until the user presses the 's' key.
+        """
+        while self.wait_for_s_key():
+
+            self.surface.fill([0, 0, 0])
             title_font = pygame.font.Font(os.path.join('.', config.font_name), 72)
             text_surface = title_font.render("CHUCKIE EGG", False, pygame.color.Color('yellow'))
             x = (config.window_width - text_surface.get_width()) // 2
             y = 120
-            window.blit(text_surface, (x, y))
+            self.surface.blit(text_surface, (x, y))
             y += text_surface.get_height()
 
-            sub_title_font = font
-            text_surface = sub_title_font.render("HIGH SCORES", False, pygame.color.Color('yellow'))
+            text_surface = self.font.render("HIGH SCORES", False, pygame.color.Color('yellow'))
             x = (config.window_width - text_surface.get_width()) // 2
             y += 20
-            window.blit(text_surface, (x, y))
+            self.surface.blit(text_surface, (x, y))
             y += text_surface.get_height()
 
             y += 40
             scores = self.scores
             for name, score in scores:
                 index = scores.index((name, score))
-                text_surface = sub_title_font.render(f"{index+1}", False, pygame.color.Color('green'))
-                window.blit(text_surface, (440, y))
-                text_surface = sub_title_font.render(f"{score}", False, pygame.color.Color('green'))
-                window.blit(text_surface, (620-text_surface.get_width(), y))
-                text_surface = sub_title_font.render(name, False, pygame.color.Color('green'))
-                window.blit(text_surface, (640, y))
+                text_surface = self.font.render(f"{index+1}", False, pygame.color.Color('green'))
+                self.surface.blit(text_surface, (440, y))
+                text_surface = self.font.render(f"{score}", False, pygame.color.Color('green'))
+                self.surface.blit(text_surface, (620-text_surface.get_width(), y))
+                text_surface = self.font.render(name, False, pygame.color.Color('green'))
+                self.surface.blit(text_surface, (640, y))
                 y += text_surface.get_height()
                 y += 10
 
-            start_one = font
-            text_surface = start_one.render("Press S to start", False, pygame.color.Color('yellow'))
+            text_surface = self.font.render("Press S to start", False, pygame.color.Color('yellow'))
             x = (config.window_width - text_surface.get_width()) // 2
             y += 40
-            window.blit(text_surface, (x, y))
+            self.surface.blit(text_surface, (x, y))
 
             pygame.display.flip()
-            clock.tick(fps)
-            ctrls.process_events(None)
-
+            self.clock.tick(config.fps)
         return
+
+
+if __name__ == '__main__':
+    surface = pygame.display.set_mode([config.window_width, config.window_height])
+    clock = pygame.time.Clock()
+    pygame.init()
+    hs = HighScores(surface, clock)
+    hs.update(19740)
