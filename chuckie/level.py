9ff14ed7526ae86a722fb0aa31c2069c48c3773e
NerdSezzer <sezzeriam@gmail.com>
2023-02-22 07:28:02 +0000

    removed start sound.  add too much noice, get it!?

--- chuckie/level.py
+++ chuckie/level.py
@@ -35,8 +35,6 @@ class Level:
         self._lifts = pygame.sprite.Group()
         self.level_egg_count = 0
         self.eggs_collected = 0
-
-        self.start = pygame.mixer.Sound(os.path.join('.', 'start.wav'))
         return
 
     def __repr__(self):
@@ -95,12 +93,6 @@ class Level:
                 if label:
                     self.tiles[(x, y)] = label
 
-            self.play_start_sound()
-        return
-
-    def play_start_sound(self):
-        self.start.play()
-        self.start.set_volume(0.5)
         return
 
     def all_landables(self):
@@ -134,7 +126,6 @@ class Level:
         """
         self._reset_moveables()
         self.status.reset_time()
-        self.play_start_sound()
         return
 
     def unload(self):
